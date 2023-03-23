bd56b1ebb3eadb6650653ad2c488c23b500fa1c5
Awesome Code <team@xinminlabs.com>
2023-03-23 11:06:40 +0000

    Auto corrected by following Format Ruby Code

--- lib/node_mutation/result.rb
+++ lib/node_mutation/result.rb
@@ -19,7 +19,10 @@ class NodeMutation::Result
   end
 
   def actions=(actions)
-    @actions = actions.map { |action| NodeMutation::Struct::Action.new(start: action.start, end: action.end, new_code: action.new_code) }
+    @actions =
+      actions.map { |action|
+        NodeMutation::Struct::Action.new(start: action.start, end: action.end, new_code: action.new_code)
+      }
   end
 
   def to_json(*args)
