bfc97ff1ffb36c7179c32d83a72e4066402bfe36
Richard Huang <flyerhzm@gmail.com>
2023-03-25 09:49:34 +0800

    test remove_comma

--- spec/node_mutation/action_spec.rb
+++ spec/node_mutation/action_spec.rb
@@ -103,4 +103,53 @@ RSpec.describe NodeMutation::Action do
       expect(action.end).to eq '%i(foo bar'.length
     end
   end
+
+  describe '#remove_comma' do
+    it 'removes comma after' do
+      action.instance_variable_set(:@file_source, 'test(foo, bar)')
+      action.instance_variable_set(:@start, 'test('.length)
+      action.instance_variable_set(:@end, 'test(foo'.length)
+      action.send(:remove_comma)
+      expect(action.start).to eq 'test('.length
+      expect(action.end).to eq 'test(foo,'.length
+    end
+
+    it 'removes comma before' do
+      action.instance_variable_set(:@file_source, 'test(foo, bar)')
+      action.instance_variable_set(:@start, 'test(foo, '.length)
+      action.instance_variable_set(:@end, 'test(foo, bar'.length)
+      action.send(:remove_comma)
+      expect(action.start).to eq 'test(foo'.length
+      expect(action.end).to eq 'test(foo, bar'.length
+    end
+
+    it 'removes comma before a newline' do
+      action.instance_variable_set(:@file_source, <<~EOS)
+        test(
+          foo: foo,
+          bar: bar
+        )
+      EOS
+      action.instance_variable_set(:@start, <<~EOS.strip.length)
+        test(
+          foo: foo,
+
+      EOS
+      action.instance_variable_set(:@end, <<~EOS.strip.length)
+        test(
+          foo: foo,
+          bar: bar
+      EOS
+      action.send(:remove_comma)
+      expect(action.start).to eq <<~EOS.strip.length
+        test(
+          foo: foo
+      EOS
+      expect(action.end).to eq <<~EOS.strip.length
+        test(
+          foo: foo,
+          bar: bar
+      EOS
+    end
+  end
 end
